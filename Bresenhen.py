684d2902fa762c3ec9fe2860da3549cbbafa87a2
ESWAR CHAND <86471473+Chandu71202@users.noreply.github.com>
2022-03-04 23:32:17 +0530

    Update Bresenhen.py

--- Bresenhen.py
+++ Bresenhen.py
@@ -5,11 +5,17 @@ def Bres(x,y,x2,y2):
     dy=y2-y1
     p=(2*dy)-dx
     while(x<=x2):
-        print(x,y)
+        print("X: ",x,"  Y: ",y)
         x=x+1;
         if(p<0):
             p=p+(2*dy)
         else:
             p=p+(2*dy)-(2*dx)
             y=y+1
-Bres(5,5,12,10)    
+
+X=int(input("Enter the value of X: "))
+Y=int(input("Enter the value of Y: "))
+X1=int(input("Enter the value of X1: "))
+Y1=int(input("Enter the value of Y1: "))
+print("The values of the Bresenhen Line Drawing Algorithms are.....")
+Bres(X,Y,X1,Y1)    

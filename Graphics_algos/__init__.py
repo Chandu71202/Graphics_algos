8a17554e97664c6038c862076c7a73443c8d7889
ESWAR CHAND <86471473+Chandu71202@users.noreply.github.com>
2022-03-05 00:00:12 +0530

    uptaded bresenhen algo

--- Graphics_algos/__init__.py
+++ Graphics_algos/__init__.py
@@ -1,3 +1,9 @@
+# Graphics_algos 
+# Copyrighted under MIT 
+# author : V.EswarChand
+# don't change anything here.
+
+
 def Bres(x,y,x2,y2):
     x1=x
     y1=y
@@ -12,10 +18,12 @@ def Bres(x,y,x2,y2):
         else:
             p=p+(2*dy)-(2*dx)
             y=y+1
-
-X=int(input("Enter the value of X: "))
-Y=int(input("Enter the value of Y: "))
-X1=int(input("Enter the value of X1: "))
-Y1=int(input("Enter the value of Y1: "))
-print("The values of the Bresenhen Line Drawing Algorithms are.....")
-Bres(X,Y,X1,Y1)    
+def Bresenhen_Algo():
+    X=int(input("Enter the value of X: "))
+    Y=int(input("Enter the value of Y: "))
+    X1=int(input("Enter the value of X1: "))
+    Y1=int(input("Enter the value of Y1: "))
+    print("The values of the Bresenhen Line Drawing Algorithms are.....")
+    Bres(X,Y,X1,Y1)   
+    
+Bresenhen_Algo()
